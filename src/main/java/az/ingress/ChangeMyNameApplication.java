7daa9ad56cb676ce17519f1b5166421bf328b520
Uncle Turgud <turgudguliyev@gmail.com>
2025-12-24 09:59:05 +0400

    Initial commit

---
+++ src/main/java/az/ingress/ChangeMyNameApplication.java
@@ -0,0 +1,13 @@
+package az.ingress;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class ChangeMyNameApplication {
+
+    public static void main(String[] args) {
+        run(ChangeMyNameApplication.class, args);
+    }
+}
