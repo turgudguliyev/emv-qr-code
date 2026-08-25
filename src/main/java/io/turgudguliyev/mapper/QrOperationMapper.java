399bcb6c878ef23c398bc9c7fe6580a2aad0fef2
Turgud F. Guliyev <GuliyevTF>
2026-08-25 09:40:49 +0400

    feat: add qr viewer

--- src/main/java/io/turgudguliyev/mapper/QrOperationMapper.java
+++ src/main/java/io/turgudguliyev/mapper/QrOperationMapper.java
@@ -1,13 +1,13 @@
 package io.turgudguliyev.mapper;
 
-import io.turgudguliyev.model.response.QrOperationResponse;
-
 import static io.turgudguliyev.util.QRCodeGenerator.QR_CODE_GENERATOR;
+
+import io.turgudguliyev.model.response.QrOperationResponse;
 
 public enum QrOperationMapper {
     QR_OPERATION_MAPPER;
 
     public QrOperationResponse toQrOperationResponse(String payload) {
-        return QrOperationResponse.of(QR_CODE_GENERATOR.generateQR(payload));
+        return QrOperationResponse.of(QR_CODE_GENERATOR.generateQR(payload), payload);
     }
 }
